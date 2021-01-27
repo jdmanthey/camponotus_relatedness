76805af7296ad349f5de68d0b4a447ff4f7e0267
Joe Manthey <jdmanthey@gmail.com>
2021-01-27 10:46:58 -0600

    Create 01_setup.sh

---
+++ 01_setup.sh
@@ -0,0 +1,47 @@
+# start an interactive session and wait to be logged in to a compute node
+interactive
+
+# move to your working directory for this project
+cd /lustre/scratch/jmanthey/04_relatedness_test/
+
+# make directories for organization during analyses
+mkdir 00_fastq
+mkdir 01_cleaned
+mkdir 01_mtDNA
+mkdir 01_bam_files
+mkdir 02_vcf
+mkdir 03_vcf
+mkdir 04_filtered_vcf
+mkdir 05_related_files
+
+#####################################
+#####################################
+#####################################
+##### transfer your raw data files for this project to the directory 00_fastq
+#####################################
+#####################################
+#####################################
+
+#####################################
+#####################################
+#####################################
+##### make a file map in the style of the file "rename_samples.txt" for your samples
+##### and put it in your 00_fastq directory
+#####################################
+#####################################
+#####################################
+
+# move to the fastq directory
+cd /lustre/scratch/jmanthey/04_relatedness_test/00_fastq/
+
+# rename the files in a numbered fashion
+while read -r name1 name2; do
+	mv $name1 $name2
+done < rename_samples.txt
+
+
+
+
+
+
+
