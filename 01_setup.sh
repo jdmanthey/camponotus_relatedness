e42efed775ee2cfeb4b1c6c96a4f2bd4af6f3060
Joe Manthey <jdmanthey@gmail.com>
2021-01-27 16:59:34 -0600

    Update 01_setup.sh

--- 01_setup.sh
+++ 01_setup.sh
@@ -28,8 +28,32 @@ mkdir 13_convert_scripts
 #####################################
 #####################################
 #####################################
-##### make a file map in the style of the file "rename_samples.txt" for your samples
+##### make a file map in the style (and same name) of the file "rename_samples.txt" for your samples
 ##### and put it in your 00_fastq directory
+#####################################
+#####################################
+#####################################
+
+#####################################
+#####################################
+#####################################
+##### make a sample name file in the style (and same name) of the file "camp_popmap.txt" for your samples
+##### and put it in the main working directory
+##### This file is essentially just the names of the samples in your study.
+##### Make sure the names and numbers are in the same order as the file map you just created.
+##### The names in the first column should be in the format "C-###"
+##### If the sample number is less than 3 digits, add the appropriate number of preceding zeros to make it three digits.
+#####################################
+#####################################
+#####################################
+
+#####################################
+#####################################
+#####################################
+##### make a mtDNA name conversion file in the style (and same name) of the file "camp_mtdna_name.txt" for your samples
+##### and put it in the 01_mtDNA directory
+##### Make sure the names and numbers are in the same order as the file map you created.
+##### The names in the first column should be in the format "C-###__mtDNA.fastq.gz"
 #####################################
 #####################################
 #####################################
@@ -41,6 +65,11 @@ cd /lustre/scratch/jmanthey/04_relatedness_test/00_fastq/
 while read -r name1 name2; do
 	mv $name1 $name2
 done < rename_samples.txt
+
+# move to your main working directory and copy the genome index file there 
+cd /lustre/scratch/jmanthey/04_relatedness_test/
+cp /home/jmanthey/denovo_genomes/camp_sp_genome_filtered.fasta.fai .
+
 
 
 
