262ccd91cedeb63b83ca28116a8e349f52291784
Joe Manthey <jdmanthey@gmail.com>
2021-01-28 09:17:40 -0600

    Create 04_setup2.sh

---
+++ 04_setup2.sh
@@ -0,0 +1,13 @@
+# move to your working directory for this project
+cd /lustre/scratch/jmanthey/04_relatedness_test/
+
+# move to the filtering script directory
+cd 12_filter_scripts
+
+################
+### modify the 05_filter_vcf.sh script as necessary (e.g., for your working directory) and put it in the current working directory (12_filter_scripts)
+################
+
+# copy one of the genotyping helper files into this directory
+cp ../11_genotype_scripts/03b_group_genotype_database/helper6.txt .
+
